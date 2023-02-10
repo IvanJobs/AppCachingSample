d931b9ec17991170e6b368a862c3aa149935a905
Ivan Jobs <ivan1377@163.com>
2023-02-10 09:58:39 +0800

    chore: enable app caching

--- tabs/src/components/Tab.jsx
+++ tabs/src/components/Tab.jsx
@@ -1,13 +1,18 @@
 import React from "react";
-import { app } from "@microsoft/teams-js";
-import MediaQuery from 'react-responsive';
+import { app, teamsCore } from "@microsoft/teams-js";
 import './App.css';
+
+function logMessage(action, message)  
+{
+  return "<span style='font-weight:bold'>" + action + "</span>" + message + "</br>";
+}
 
 class Tab extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-      context: {}
+      context: {},
+      logs: []
     }
   }
 
@@ -17,10 +22,38 @@ class Tab extends React.Component {
     app.initialize().then(() => {
       // Get the user context from Teams and set it in the state
       app.getContext().then(async (context) => {
+        app.notifySuccess();
+        this.setState({
+          logs: [...this.state.logs, logMessage("OnAppContext", "Notify success of loading - Loaded Teams context.")]
+        });
+
         this.setState({
           meetingId: context.meeting.id,
           userName: context.user.userPrincipalName
         });
+
+        // App caching is only available in meeting's side panel.
+        if (context.page.frameContext === "sidePanel") {
+          teamsCore.registerBeforeUnloadHandler((readyToUnload: any) => {
+            this.setState({
+              logs: [...this.state.logs, logMessage("BeforeUnload", "Sending readyToUnload to Teams...")]
+            });
+            readyToUnload();
+            return true;
+          });
+
+          teamsCore.registerOnLoadHandler((context) => {
+            this.setState({
+              logs: [...this.state.logs, logMessage("OnLoadContext", "Entity Id: " + context.entityId + ", Content Url: " + context.contentUrl)]
+            });
+
+            app.notifySuccess();
+          });
+
+          this.setState({
+            logs: [...this.state.logs, logMessage("Summary", "Registered load & beforeUnload handlers. Ready for app caching.")]
+          });
+        }
       });
     });
     // Next steps: Error handling using the error object
@@ -29,19 +62,19 @@ class Tab extends React.Component {
   render() {
     let meetingId = this.state.meetingId ?? "";
     let userPrincipleName = this.state.userName ?? "";
-
+    let logs = this.state.logs;
     return (
     <div>
       <h1>In-meeting app sample</h1>
       <h3>Principle Name:</h3>
       <p>{userPrincipleName}</p>
       <h3>Meeting ID:</h3>
-      <p>{meetingId}</p>
-      
-      <MediaQuery maxWidth={280}>
-        <h3>This is the side panel</h3>
-        <a href="https://docs.microsoft.com/en-us/microsoftteams/platform/apps-in-teams-meetings/teams-apps-in-meetings">Need more info, open this document in new tab or window.</a>
-      </MediaQuery>
+      <p>{meetingId}</p> 
+      <div>
+        {logs.map((log) => {
+          return <div dangerouslySetInnerHTML={{ __html: log }}></div>
+        })}
+      </div>
     </div>
     );
   }
